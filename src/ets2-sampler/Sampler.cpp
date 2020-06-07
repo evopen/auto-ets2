10bb95901566cec3e9940906073395d4386b9201
evopen <520dhh@gmail.com>
2020-06-07 10:52:38 +0800

    add a game sampler

---
+++ src/ets2-sampler/Sampler.cpp
@@ -0,0 +1,32 @@
+#include <ScreenCapturer.h>
+#include <opencv2/opencv.hpp>
+
+#include <chrono>
+#include <iostream>
+
+
+const int kSampleTime = 1000;  // millisecond
+
+int main()
+{
+    cv::Mat screenshot;
+    ScreenCapturer capturer;
+    capturer.Init();
+
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::duration<double, std::milli>(kSampleTime));
+        screenshot = capturer.Capture();
+        std::string filename(std::to_string(
+            std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
+                .count()));
+        std::cout << filename << std::endl;
+        cv::imwrite("images/" + filename + ".png", screenshot);
+        if (cv::waitKey(1) == 27)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
